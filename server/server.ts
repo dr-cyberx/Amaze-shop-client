8735ee69974c4160ac54023c2948a0f25435fa71
ItsMeVishal007 <vishal.codedrill@gmail.com>
2022-01-06 18:59:37 +0530

    Changes

--- server/server.ts
+++ server/server.ts
@@ -1,7 +1,10 @@
 import express, { Application } from 'express';
+import dotenv from 'dotenv';
 import Resolvers from '../resolvers/index';
 import TypeDefs from '../typedefs/index';
 import startApolloServer from './initializeServer';
+
+dotenv.config();
 
 const app: Application = express();
 
