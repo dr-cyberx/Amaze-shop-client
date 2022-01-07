b850acccaee50790257eb86b9b5b8d03903b5e37
ItsMeVishal007 <vishal.codedrill@gmail.com>
2022-01-07 18:26:29 +0530

    changes in app client side

--- resolvers/auth/signUp.ts
+++ resolvers/auth/signUp.ts
@@ -1,9 +1,9 @@
-import SignUp from '../../controllers/User';
+import SignUp from '../../controllers/auth';
 import { SingleuserType } from '../../types/userType';
 
 export const hello = () => 'hello';
 
-export const signUp = (_parents: any, args: SingleuserType, context: any) => {
+export const signUp = (_parents: any, args: SingleuserType) => {
   const newUser = SignUp(args);
   return newUser;
 };
