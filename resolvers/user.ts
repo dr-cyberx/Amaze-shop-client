3c1e00c12dbdd5730efd2021e77f7b12addf6724
ItsMeVishal007 <vishal.codedrill@gmail.com>
2022-01-11 18:22:54 +0530

    changes in type schema

--- resolvers/user.ts
+++ resolvers/user.ts
@@ -1,16 +1,7 @@
-import User from '../models/User';
-import { findFromDB } from '../shared/user';
-import { SingleuserType } from '../types/userType';
+import { getAllUser } from '../controllers/user';
 
 const UserQuery = {
-  getAllUser: async (): Promise<any[] | SingleuserType | null> => {
-    try {
-      const res: Promise<any[] | SingleuserType> = findFromDB(User, 'All');
-      return res;
-    } catch (err) {
-      return null;
-    }
-  },
+  getAllUser,
 };
 
 export default UserQuery;
--- types/userType.ts
+++ types/userType.ts
@@ -1,7 +1,10 @@
+import mongoose from 'mongoose';
+
 export interface SingleuserType {
+  id?:mongoose.Types.ObjectId,
   userName: String;
   email: String;
-  password: String;
+  password?: String;
   phoneNumber: String;
   address?: { houseNumber: String; city: String; street: String }[];
 }
