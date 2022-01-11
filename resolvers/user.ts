88438fe14dabc10a9c4c420fc462a31a0a0e05fa
ItsMeVishal007 <vishal.codedrill@gmail.com>
2022-01-11 11:26:33 +0530

    changes

--- resolvers/user.ts
+++ resolvers/user.ts
@@ -1,11 +1,11 @@
 import User from '../models/User';
-import { findAllFromDB } from '../shared/user';
+import { findFromDB } from '../shared/user';
 import { SingleuserType } from '../types/userType';
 
 const UserQuery = {
   getAllUser: async (): Promise<SingleuserType[] | null> => {
     try {
-      const res: Promise<SingleuserType[]> = findAllFromDB(User);
+      const res: Promise<SingleuserType[]> = findFromDB(User, 'All');
       return res;
     } catch (err) {
       return null;
