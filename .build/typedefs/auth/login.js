e13cc06bb1a2ecb627c8c36a661a5db7367df818
ItsMeVishal007 <vishal.codedrill@gmail.com>
2022-01-12 13:35:49 +0530

    changes

--- .build/typedefs/auth/login.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Login = "\nLogin(userName: String, email: String, phoneNumber: String, password: String) : String!\n";
-exports.default = Login;
