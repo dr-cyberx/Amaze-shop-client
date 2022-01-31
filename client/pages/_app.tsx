af2a1afba73354261a10811c5e5f4dfccb841a39
ItsMeVishal007 <vishal.codedrill@gmail.com>
2022-01-31 18:39:36 +0530

    changes

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,8 +1,23 @@
-import '../styles/globals.css'
+import React from 'react';
 import type { AppProps } from 'next/app'
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+  gql
+} from "@apollo/client";
+import '../styles/globals.css'
+
+const client = new ApolloClient({
+  uri: 'https://48p1r2roz4.sse.codesandbox.io',
+  cache: new InMemoryCache()
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <>
+  <Component {...pageProps} />
+  </> 
 }
 
 export default MyApp
