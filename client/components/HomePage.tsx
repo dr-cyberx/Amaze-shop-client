ba2555c0eacc6d016aaf5e27a65292687d048c71
@vishal.kumar <vishal.kumar@growthnatives.com>
2022-03-25 18:28:03 +0530

    workingn on crousel on home page

--- client/components/HomePage.tsx
+++ client/components/HomePage.tsx
@@ -7,15 +7,19 @@ import styles from '@styles/HomePage.module.scss';
 const HomePageCrouselContent = [
   {
     id: 0,
-    content: <img src="/home1.webp" alt="image 1" />,
+    content: <img src="/4ktv.jpg" alt="image 1" />,
   },
   {
     id: 1,
-    content: <img src="/home2.webp" alt="image 1" />,
+    content: <img src="/console2.jpg" alt="image 2" />,
   },
   {
     id: 2,
-    content: <img src="/home3.webp" alt="image 1" />,
+    content: <img src="/laptop.jpg" alt="image 3" />,
+  },
+  {
+    id: 3,
+    content: <img src="/phone.jpg" alt="image 4" />,
   },
 ];
 
@@ -26,9 +30,10 @@ const HomePage: React.FunctionComponent = (): JSX.Element => {
         <AmazeCrousel
           DataList={HomePageCrouselContent}
           animationDuration={2000}
-          navButtonsAlwaysVisible
+          // navButtonsAlwaysVisible
           navButtonsAlwaysInvisible={false}
           autoPlay={true}
+          animationInterval={3000}
         />
       </div>
     </Layout>
